0b5ecd3aa516fb299fc48141ddd719afcd92f88c
Matt Clark <matthew.clark@undp.org>
2016-08-26 15:38:18 +1000

    Updated NAP-GSP Home Page

--- templates/system/page--front.tpl.php
+++ templates/system/page--front.tpl.php
@@ -1,98 +1,42 @@
-<?php
-
-/**
- * @file
- * Default theme implementation to display a single Drupal page.
- *
- * The doctype, html, head and body tags are not in this template. Instead they
- * can be found in the html.tpl.php template in this directory.
- *
- * Available variables:
- *
- * General utility variables:
- * - $base_path: The base URL path of the Drupal installation. At the very
- *   least, this will always default to /.
- * - $directory: The directory the template is located in, e.g. modules/system
- *   or themes/bartik.
- * - $is_front: TRUE if the current page is the front page.
- * - $logged_in: TRUE if the user is registered and signed in.
- * - $is_admin: TRUE if the user has permission to access administration pages.
- *
- * Site identity:
- * - $front_page: The URL of the front page. Use this instead of $base_path,
- *   when linking to the front page. This includes the language domain or
- *   prefix.
- * - $logo: The path to the logo image, as defined in theme configuration.
- * - $site_name: The name of the site, empty when display has been disabled
- *   in theme settings.
- * - $site_slogan: The slogan of the site, empty when display has been disabled
- *   in theme settings.
- *
- * Navigation:
- * - $main_menu (array): An array containing the Main menu links for the
- *   site, if they have been configured.
- * - $secondary_menu (array): An array containing the Secondary menu links for
- *   the site, if they have been configured.
- * - $breadcrumb: The breadcrumb trail for the current page.
- *
- * Page content (in order of occurrence in the default page.tpl.php):
- * - $title_prefix (array): An array containing additional output populated by
- *   modules, intended to be displayed in front of the main title tag that
- *   appears in the template.
- * - $title: The page title, for use in the actual HTML content.
- * - $title_suffix (array): An array containing additional output populated by
- *   modules, intended to be displayed after the main title tag that appears in
- *   the template.
- * - $messages: HTML for status and error messages. Should be displayed
- *   prominently.
- * - $tabs (array): Tabs linking to any sub-pages beneath the current page
- *   (e.g., the view and edit tabs when displaying a node).
- * - $action_links (array): Actions local to the page, such as 'Add menu' on the
- *   menu administration interface.
- * - $feed_icons: A string of all feed icons for the current page.
- * - $node: The node object, if there is an automatically-loaded node
- *   associated with the page, and the node ID is the second argument
- *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
- *   comment/reply/12345).
- *
- * Regions:
- * - $page['help']: Dynamic help text, mostly for admin pages.
- * - $page['highlighted']: Items for the highlighted content region.
- * - $page['content']: The main content of the current page.
- * - $page['sidebar_first']: Items for the first sidebar.
- * - $page['sidebar_second']: Items for the second sidebar.
- * - $page['header']: Items for the header region.
- * - $page['footer']: Items for the footer region.
- *
- * @see template_preprocess()
- * @see template_preprocess_page()
- * @see template_process()
- * @see html.tpl.php
- *
- * @ingroup themeable
- */
-?>
-<div id="page-wrapper">
-    <div id="page">
-        <div class="flex-container">
-            <a href="/nap-gsp" class="image-container">
-                <div id="image-01" >
-                    <img src="/sites/all/themes/custom/nap/assets/img/cover-image-01.jpg" />
-                    <div class="overlay">
-                        <div class="gsp-logo-container">
-                            <div class="logo"></div>
-                        </div>
-                    </div>
-                </div>
-            </a>
-            <a href="/ldc-negotiators" class="image-container">
-                <div id="image-02">
-                    <img src="/sites/all/themes/custom/nap/assets/img/cover-image-02.jpg" />
-                    <div class="overlay">
-                        <h1>LDC Negotiators</h1>
-                    </div>
-                </div>
-            </a>
+<div class="page-wrapper">
+    <div class="header">
+        <div class="container">
+            <h2>Global Support Programme</h2>
+            <p>The UN Development Programme and the UN Environment Programme are working jointly on two Global Support Programmes which assist developing countries to adapt to climate change. This UNDP-UNEP collaboration is supporting more than 40 of the most vulnerable countries as they advance their climate change resilience.</p>
+            <ul class="gsp-logos">
+                <li><img src="/sites/all/themes/custom/nap/assets/img/logos/undp.svg" /></li>
+                <li><img src="/sites/all/themes/custom/nap/assets/img/logos/unep.svg" /></li>
+            </ul>
         </div>
     </div>
-</div> <!-- /#page, /#page-wrapper -->
+    <div class="content">
+        <div class="container">
+            <h3>Supported Programmes</h3>
+            <ul class="supported-programmes">
+                <li>
+                    <img src="/sites/all/themes/custom/nap/assets/img/cover-image-01.jpg" />
+                    <div class="overlay">
+                        <div class="logo-container gsp-logo-container">
+                            <div class="logo"></div>
+                            <p>National Adaptation Plan Global Support Programme (NAP-GSP) is a joint UNDP-UNEP programme to assist developing countries to advance National Adaptation Plans (NAPs).</p>
+                            <a class="btn btn-learn-more" href="/nap-gsp">Learn More</a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <a href="/ldc-negotiators">
+                        <img src="/sites/all/themes/custom/nap/assets/img/cover-image-02.jpg" />
+                    </a>
+                    <div class="overlay">
+                        <div class="logo-container ldc-negotiators-logo-container">
+                            <div class="logo"></div>
+                            <p>The programme Building Capacity for Least Developed Countries (LDCs) to Participate Effectively in Intergovernmental Climate Change Processes (LDC Negotiators) is a joint UNDP-UNEP global support programme to assist LDCs with climate change negotiations.</p>
+                            <a class="btn btn-learn-more" href="/ldc-negotiators">Learn More</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
